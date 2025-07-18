a33d8b88bfcc51725a63ea4e9c4818bd745e795e
Angel Padrino <angel.padrino04@gmail.com>
2025-07-17 20:20:31 -0400

    fix

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
 import os
-
 from training import train_and_evaluate
 
 os.makedirs('results', exist_ok=True)
